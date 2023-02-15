1daf19be49b97e443b598645f2e53876600b8198
Mike Churvis <mikechurvis@gmail.com>
2023-02-15 21:57:37 +0000

    Add app state def and reducer

---
+++ src/appState.ts
@@ -0,0 +1,36 @@
+type AppState = {
+  clicks: number;
+  usersOnline: number;
+};
+
+type IncrementClicksAction = {
+  type: "increment_clicks";
+};
+
+type SetUsersOnlineAction = {
+  type: "set_users_online";
+  usersOnline: number;
+};
+
+type SetClicksAction = {
+  type: "set_clicks";
+  clicks: number;
+};
+
+type Action = IncrementClicksAction | SetUsersOnlineAction | SetClicksAction;
+
+function appStateReducer(state: AppState, action: Action): AppState {
+  switch (action.type) {
+    case "increment_clicks":
+      return { ...state, clicks: state.clicks + 1 };
+    case "set_users_online":
+      return { ...state, usersOnline: action.usersOnline };
+    case "set_clicks":
+      return { ...state, clicks: action.clicks };
+    default:
+      return state;
+  }
+}
+
+export default { appStateReducer };
+export type { AppState };
