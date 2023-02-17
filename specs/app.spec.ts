4f953be0dcc771a5dbded1250e034378e0aca00c
Mike Churvis <mikechurvis@gmail.com>
2023-02-17 22:35:56 +0000

    Update tests
    Test headers are in place.

---
+++ specs/app.spec.ts
@@ -0,0 +1,73 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("What the user sees when they launch the app:", () => {
+  test("A header with instruction subtext.", () => {
+    test.fail();
+  });
+
+  test("A button with the Python Atlanta logo on it.", () => {
+    test.fail();
+  });
+
+  test("A label that says 'click me', pointing at the button.", () => {
+    test.fail();
+  });
+
+  test("The number of times the button has been clicked by all users, labeled as such.", () => {
+    test.fail();
+  });
+
+  test("The number of clickers online, including themself.", () => {
+    test.fail();
+  });
+
+  test("A link to the sign-up page for the PyATL Jam session in April.", () => {
+    test.fail();
+  });
+
+  test("A link to my website.", () => {
+    test.fail();
+  });
+});
+
+test.describe("How the user can interact with the button:", () => {
+  test("Click the button with the left mouse button.", () => {
+    test.fail();
+  });
+
+  test("Click the button by tapping it.", () => {
+    test.fail();
+  });
+
+  test("Focus the button with the keyboard.", () => {
+    test.fail();
+  });
+
+  test("Click the button by focusing it and pressing ENTER or SPACE.", () => {
+    test.fail();
+  });
+});
+
+test.describe("What happens when the user clicks the button:", () => {
+  test("The number upon the button increments by 1.", () => {
+    test.fail();
+  });
+
+  test("The label that says 'click me' disappears.", () => {
+    test.fail();
+  });
+});
+
+test.describe("What the user sees when a another user does things:", () => {
+  test("The clicks counter goes up by 1 when a another user clicks the button.", () => {
+    test.fail();
+  });
+
+  test("The clickers online counter goes up by 1 when another user launches the app.", () => {
+    test.fail();
+  });
+
+  test("The clickers online counter goes down by 1 when another user, currently using the app, closes the app.", () => {
+    test.fail();
+  });
+});
