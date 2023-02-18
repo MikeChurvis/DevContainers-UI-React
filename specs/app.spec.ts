d25a21e1a322a9f37d897c28ebd990291df78d79
Mike Churvis <mikechurvis@gmail.com>
2023-02-18 02:21:26 +0000

    Update app spec tests
    Made test functions async.
    Functions now take page fixtures as they should.
    Fleshed out the first test; it fails.
    TODO: update dev container to install playwright deps on setup.

--- specs/app.spec.ts
+++ specs/app.spec.ts
@@ -1,82 +1,109 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("What the user sees when they launch the app:", () => {
-  test("A header with instruction subtext.", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+
+  test("A header with instruction subtext.", async ({ page }) => {
+    await page.locator("heading").getByRole("heading");
+    const instructionSubtext = await page.locator("heading");
+
+    expect(instructionSubtext).toHaveText(/click.*button/);
+  });
+
+  test("A button with the Python Atlanta logo on it.", async ({ page }) => {
     test.fail();
   });
 
-  test("A button with the Python Atlanta logo on it.", () => {
+  test("A label that says 'click me', pointing at the button.", async ({
+    page,
+  }) => {
     test.fail();
   });
 
-  test("A label that says 'click me', pointing at the button.", () => {
+  test("The number of times the button has been clicked by all users, labeled as such.", async ({
+    page,
+  }) => {
     test.fail();
   });
 
-  test("The number of times the button has been clicked by all users, labeled as such.", () => {
+  test("The number of clickers online, including themself.", async ({
+    page,
+  }) => {
     test.fail();
   });
 
-  test("The number of clickers online, including themself.", () => {
+  test("A link to the sign-up page for the PyATL Jam session in April.", async ({
+    page,
+  }) => {
     test.fail();
   });
 
-  test("A link to the sign-up page for the PyATL Jam session in April.", () => {
-    test.fail();
-  });
-
-  test("A link to my website.", () => {
+  test("A link to my website.", async ({ page }) => {
     test.fail();
   });
 });
 
 test.describe("How the user can interact with the button:", () => {
-  test("Click the button with the left mouse button.", () => {
+  test("Click the button with the left mouse button.", async ({ page }) => {
     test.fail();
   });
 
-  test("Click the button by tapping it.", () => {
+  test("Click the button by tapping it.", async ({ page }) => {
     test.fail();
   });
 
-  test("Focus the button with the keyboard.", () => {
+  test("Focus the button with the keyboard.", async ({ page }) => {
     test.fail();
   });
 
-  test("Click the button by focusing it and pressing ENTER or SPACE.", () => {
+  test("Click the button by focusing it and pressing ENTER or SPACE.", async ({
+    page,
+  }) => {
     test.fail();
   });
 });
 
 test.describe("What happens when the user clicks the button:", () => {
-  test("The number upon the button increments by 1.", () => {
+  test("The number upon the button increments by 1.", async ({ page }) => {
     test.fail();
   });
 
-  test("The label that says 'click me' disappears.", () => {
+  test("The label that says 'click me' disappears.", async ({ page }) => {
     test.fail();
   });
 });
 
 test.describe("What the user sees when a another user does things:", () => {
-  test("The clicks counter goes up by 1 when a another user clicks the button.", () => {
+  test("The clicks counter goes up by 1 when a another user clicks the button.", async ({
+    page,
+  }) => {
     test.fail();
   });
 
-  test("The clickers online counter goes up by 1 when another user launches the app.", () => {
+  test("The clickers online counter goes up by 1 when another user launches the app.", async ({
+    page,
+  }) => {
     test.fail();
   });
 
-  test("The clickers online counter goes down by 1 when another user, currently using the app, closes the app.", () => {
+  test("The clickers online counter goes down by 1 when another user, currently using the app, closes the app.", async ({
+    page,
+  }) => {
     test.fail();
   });
 
   test.describe("What happens when the user clicks the external links:", () => {
-    test("The link to the PyATL April Jam session opens (meetup.com/...) in a new tab.", () => {
+    test("The link to the PyATL April Jam session opens (meetup.com/...) in a new tab.", async ({
+      page,
+    }) => {
       test.fail();
     });
 
-    test("The link to my website opens (mikechurvis.com) in a new tab.", () => {
+    test("The link to my website opens (mikechurvis.com) in a new tab.", async ({
+      page,
+    }) => {
       test.fail();
     });
   });
